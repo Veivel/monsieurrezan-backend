911fa7c7e814e086027d15d9da5b4eba6400d25d
Veivel <25278964+Veivel@users.noreply.github.com>
2022-12-19 09:56:13 +0700

    initial commit: test deployment

---
+++ src/api/landing-hero-slide/controllers/landing-hero-slide.ts
@@ -0,0 +1,7 @@
+/**
+ * landing-hero-slide controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::landing-hero-slide.landing-hero-slide');
---
+++ src/api/landing-hero-slide/services/landing-hero-slide.ts
@@ -0,0 +1,7 @@
+/**
+ * landing-hero-slide service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::landing-hero-slide.landing-hero-slide');
